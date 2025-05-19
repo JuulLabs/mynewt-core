1f82d99f1931c5041964aaaf10583fb5cd20aeac
Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>
2025-05-19 14:21:48 +0200

    kernel/os/arch: Fix Cortex-M irq priority setting
    For Cortex-M0:
    os_arch_os_init() initialized interrupt priority level
    for all interrupts using incorrect array limit taken
    from sizeof instead of ARRAY_SIZE.
    For Cortex-M0 field IP is array of uint32_t unlike all
    other version where this array consist of uint8_t.
    
    Now array is iterated using ARRAY_SIZE(NVIC->IP) to limit
    instead of sizeof(NVIC->IP) and for Cortex-M33
    ARRAY_SIZE(NVIC->IPR).
    
    Signed-off-by: Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>

--- hw/mcu/dialog/cmac/src/arch/cortex_m0_cmac/os_arch_arm.c
+++ hw/mcu/dialog/cmac/src/arch/cortex_m0_cmac/os_arch_arm.c
@@ -17,38 +17,13 @@
  * under the License.
  */
 
-#include "os/mynewt.h"
-#include <hal/hal_bsp.h>
-#include <hal/hal_os_tick.h>
-#include <mcu/cmsis_nvic.h>
-
-#include "os_priv.h"
-
-/*
- * From HAL_CM4.s
- */
-extern void SVC_Handler(void);
-extern void PendSV_Handler(void);
-extern void SysTick_Handler(void);
+#include "mcu/mcu.h"
+#include "hal/hal_os_tick.h"
+#include "os/os_arch_cmac.h"
+#include "../../os/src/os_priv.h"
 
 /* Initial program status register */
 #define INITIAL_xPSR    0x01000000
-
-/*
- * Initial LR indicating basic frame.
- * See ARMv7-M Architecture Ref Manual
- */
-#define INITIAL_LR      0xfffffffd;
-
-/*
- * Exception priorities. The higher the number, the lower the priority. A
- * higher priority exception will interrupt a lower priority exception.
- */
-#define PEND_SV_PRIO    ((1 << __NVIC_PRIO_BITS) - 1)
-#define OS_TICK_PRIO    (PEND_SV_PRIO - 1)
-
-/* Make the SVC instruction highest priority */
-#define SVC_PRIO        (1)
 
 /* Stack frame structure */
 struct stack_frame {
@@ -60,9 +35,6 @@ struct stack_frame {
     uint32_t    r9;
     uint32_t    r10;
     uint32_t    r11;
-#if MYNEWT_VAL(HARDFLOAT)
-    uint32_t    exc_lr;
-#endif
     uint32_t    r0;
     uint32_t    r1;
     uint32_t    r2;
@@ -82,7 +54,6 @@ struct stack_frame {
   SVC_ArgN(2)       \
   SVC_ArgN(3)
 
-#if (defined (__CORTEX_M0)) || defined (__CORTEX_M0PLUS)
 #define SVC_Call(f)                                                     \
   __asm volatile                                                        \
   (                                                                     \
@@ -93,26 +64,9 @@ struct stack_frame {
     :                "r" (__r0),  "r" (__r1),  "r" (__r2),  "r" (__r3)  \
     : "r7", "r12", "lr", "cc"                                           \
   );
-#else
-#define SVC_Call(f)                                                     \
-  __asm volatile                                                        \
-  (                                                                     \
-    "ldr r12,="#f"\n\t"                                                 \
-    "svc 0"                                                             \
-    :               "=r" (__r0), "=r" (__r1), "=r" (__r2), "=r" (__r3)  \
-    :                "r" (__r0),  "r" (__r1),  "r" (__r2),  "r" (__r3)  \
-    : "r12", "lr", "cc"                                                 \
-  );
-#endif
 
 /* XXX: determine how we will deal with running un-privileged */
 uint32_t os_flags = OS_RUN_PRIV;
-
-void
-timer_handler(void)
-{
-    os_time_advance(1);
-}
 
 void
 os_arch_ctx_sw(struct os_task *t)
@@ -120,34 +74,7 @@ os_arch_ctx_sw(struct os_task *t)
     os_sched_ctx_sw_hook(t);
 
     /* Set PendSV interrupt pending bit to force context switch */
-    SCB->ICSR = SCB_ICSR_PENDSVSET_Msk;
-}
-
-os_sr_t
-os_arch_save_sr(void)
-{
-    uint32_t isr_ctx;
-
-    isr_ctx = __get_PRIMASK();
-    __disable_irq();
-    return (isr_ctx & 1);
-}
-
-void
-os_arch_restore_sr(os_sr_t isr_ctx)
-{
-    if (!isr_ctx) {
-        __enable_irq();
-    }
-}
-
-int
-os_arch_in_critical(void)
-{
-    uint32_t isr_ctx;
-
-    isr_ctx = __get_PRIMASK();
-    return (isr_ctx & 1);
+    os_arch_cmac_pendsvset();
 }
 
 os_stack_t *
@@ -158,7 +85,7 @@ os_arch_task_stack_init(struct os_task *t, os_stack_t *stack_top, int size)
     struct stack_frame *sf;
 
     /* Get stack frame pointer */
-    s = (os_stack_t *) ((uint8_t *) stack_top - sizeof(*sf));
+    s = (os_stack_t *)((uint8_t *) stack_top - sizeof(*sf));
 
     /* Zero out R1-R3, R12, LR */
     for (i = 9; i < 14; ++i) {
@@ -173,20 +100,13 @@ os_arch_task_stack_init(struct os_task *t, os_stack_t *stack_top, int size)
     sf->xpsr = INITIAL_xPSR;
     sf->pc = (uint32_t)t->t_func;
     sf->r0 = (uint32_t)t->t_arg;
-#if MYNEWT_VAL(HARDFLOAT)
-    sf->exc_lr = INITIAL_LR;
-#endif
 
-    return (s);
+    return s;
 }
 
 void
 os_arch_init(void)
 {
-    /*
-     * Trap on divide-by-zero.
-     */
-    SCB->CCR |= SCB_CCR_DIV_0_TRP_Msk;
     os_init_idle_task();
 }
 
@@ -201,50 +121,28 @@ svc_os_arch_init(void)
 os_error_t
 os_arch_os_init(void)
 {
-    os_error_t err;
     int i;
 
-    /* Cannot be called within an ISR */
-    err = OS_ERR_IN_ISR;
-    if (__get_IPSR() == 0) {
-        err = OS_OK;
-
-        /* Drop priority for all interrupts */
-        for (i = 0; i < ARRAY_SIZE(NVIC->IP); i++) {
-            NVIC->IP[i] = -1;
-        }
-
-        /*
-         * Install default interrupt handler for all interrupts except Reset,
-         * which'll print out system state at the time of the interrupt, and
-         * few other regs which should help in trying to figure out what went
-         * wrong.
-         */
-        for (i = -NVIC_USER_IRQ_OFFSET + 2;
-             i < NVIC_NUM_VECTORS - NVIC_USER_IRQ_OFFSET; i++) {
-            NVIC_SetVector(i, (uint32_t)os_default_irq_asm);
-        }
-
-        /* Install our system interrupt handlers */
-        NVIC_SetVector(SVCall_IRQn, (uint32_t)SVC_Handler);
-        NVIC_SetVector(PendSV_IRQn, (uint32_t)PendSV_Handler);
-        NVIC_SetVector(SysTick_IRQn, (uint32_t)SysTick_Handler);
-
-        /* Set the PendSV interrupt exception priority to the lowest priority */
-        NVIC_SetPriority(PendSV_IRQn, PEND_SV_PRIO);
-
-        /* Set the SVC interrupt to priority 0 (highest configurable) */
-        NVIC_SetPriority(SVCall_IRQn, SVC_PRIO);
-
-        /* Check if privileged or not */
-        if ((__get_CONTROL() & 1) == 0) {
-            os_arch_init();
-        } else {
-            svc_os_arch_init();
-        }
+    if (__get_IPSR() != 0) {
+        return OS_ERR_IN_ISR;
     }
 
-    return err;
+    /* Drop priority for all interrupts */
+    for (i = 0; i < ARRAY_SIZE(NVIC->IP); i++) {
+        NVIC->IP[i] = -1;
+    }
+
+    NVIC_SetPriority(PendSV_IRQn, -1);
+    NVIC_SetPriority(SVC_IRQ_NUMBER, 0);
+
+    /* Check if privileged or not */
+    if ((__get_CONTROL() & 1) == 0) {
+        os_arch_init();
+    } else {
+        svc_os_arch_init();
+    }
+
+    return OS_OK;
 }
 
 uint32_t
@@ -259,8 +157,12 @@ os_arch_start(void)
     /* Adjust PSP so it looks like this task just took an exception */
     __set_PSP((uint32_t)t->t_stackptr + offsetof(struct stack_frame, r0));
 
-    /* Intitialize and start system clock timer */
-    os_tick_init(OS_TICKS_PER_SEC, OS_TICK_PRIO);
+    /*
+     * Initialize and start system clock timer. The interrupt priority does not
+     * matter here since it always runs on ll_timer which has predefined
+     * priority in cmac_timer.
+     */
+    os_tick_init(OS_TICKS_PER_SEC, 0);
 
     /* Mark the OS as started, right before we run our first task */
     g_os_started = 1;
@@ -282,7 +184,6 @@ static inline void svc_os_arch_start(void)
  * Start the OS. First check to see if we are running with the correct stack
  * pointer set (PSP) and privilege mode (PRIV).
  *
- *
  * @return os_error_t
  */
 os_error_t
@@ -293,7 +194,7 @@ os_arch_os_start(void)
     /*
      * Set the os environment. This will set stack pointers and, based
      * on the contents of os_flags, will determine if the tasks run in
-     * privileged or un-privileged mode.
+     * priviliged or un-privileged mode.
      *
      * We switch to using "empty" part of idle task's stack until
      * the svc_os_arch_start() executes SVC, and we will never return.
